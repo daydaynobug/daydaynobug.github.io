9a7e5737ec7460ae1528d0790b4f64f0e0b77805
zss <zhaosongsong@wildroi.com>
2018-06-05 11:42:39 +0800

    index.php

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<?php
+<?php 
 	function checkSignature(){
 		$signature = $_GET["signature"];
+		
 		$timestamp = $_GET["timestamp"];
 		$nonce = $_GET["nonce"];
-		$echostr = $_GET["echostr"];
 		$token = "zss";
 
-		$tmpArr = array($token,$timestamp,$nonce);
-		sort($tmpArr,SORT_STRING);
-		$str = implode($tmpArr);
-		$sign = sha1($str);
+		$echostr = $_GET["echostr"];
 
+		$tmpArr = array($token,$timestamp,$nonce);
+		//排序
+		sort($tmpArr,SORT_STRING);
+		//数组转字符串
+		$str = implode($tmpArr);
+		//加密
+		$sign = sha1($str);
+		//判断是否来源于微信
 		if ($sign == $signature) {
 			echo $echostr;
 		}
 	}
-
-	checkSignature()
-
-?>
+	checkSignature();
+ ?>
