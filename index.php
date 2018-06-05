7029070fc8dbef9aa1325f7894318cd68b2f1b97
zss <zhaosongsong@wildroi.com>
2018-06-05 11:13:08 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	function checkSignature(){
+		$signature = $_GET["signature"];
+		$timestamp = $_GET["timestamp"];
+		$nonce = $_GET["nonce"];
+		$echostr = $_GET["echostr"];
+		$token = "zss";
+
+		$tmpArr = array($token,$timestamp,$nonce);
+		sort($tmpArr,SORT_STRING);
+		$str = implode($tmpArr);
+		$sign = sha1($str);
+
+		if ($sign == $signature) {
+			echo $echostr;
+		}
+	}
+
+	checkSignature()
+
+?>
