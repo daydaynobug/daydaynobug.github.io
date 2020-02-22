9d219e05258ad34aa391eec7bc3e2a71da2a87f1
zss <zhaosongsong@wildroi.com>
2020-02-22 21:41:54 +0800

    jixu

--- common/common.js
+++ common/common.js
@@ -1,3 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a13011faf0a28bb07ae3b4b10e3e52ad";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
 $(document).ready(function () {
 	$('.list .item').eq(1).hover(function(){
 	    $(".down").show()
@@ -11,4 +18,9 @@ $(document).ready(function () {
 	    500);
 	    return false;
 	});
+    $(".shangqiao").click(function(event) {
+        if ($('#nb_icon_wrap').length > 0) {
+            $('#nb_icon_wrap').click();
+        }
+    });
 })
---
+++ express/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+
+app.use(express.static('project'))
+
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
+app.get('/getArticleList',function(req,res){
+	let query = req.query
+	let data = JSON.parse(fs.readFileSync("./articleList.json").toString())
+	let page = data.totalCount % 10 == 0 ? parseInt(data.totalCount / 10) : parseInt(data.totalCount / 10) + 1
+	if(query.page == page) {
+		res.send({
+			totalCount: data.totalCount,
+			items: data.items.slice(0, 5)
+		})
+	}else {
+		res.send({
+			totalCount: data.totalCount,
+			items: data.items
+		})
+	}
+})
+
+app.listen(3000, () => console.log('app listening on port 3000!'))
